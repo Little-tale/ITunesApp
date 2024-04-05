bb1e4554f23453815a8507f5e888e95a61062031
judge_happily <you3192001@gmail.com>
2024-04-05 21:08:22 +0900

    fetch: URLRouter

---
+++ ITunesApp/Network/ITunesRouter.swift
@@ -0,0 +1,57 @@
+//
+//  ITunesRouter.swift
+//  ITunesApp
+//
+//  Created by Jae hyung Kim on 4/5/24.
+//
+
+import Foundation
+import Alamofire
+
+
+enum ITunesRouter: URLRequestConvertible {
+    
+    case search(term: String)
+    
+    var baseUrl: URL {
+        switch self {
+        case .search:
+            return URL(string: "https://itunes.apple.com/search?")!
+        }
+    }
+    
+    var path: String {
+        switch self{
+        case .search:
+            "search"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .search:
+           return .get
+        }
+    }
+    
+    var parametters: Parameters? {
+        switch self {
+        case .search(let search) :
+            return [
+                "term" : search,
+                "country" : "KR",
+                "entity" : "software"
+            ]
+        }
+        
+    }
+    
+    
+    func asURLRequest() throws -> URLRequest {
+        let url = baseUrl.appendingPathComponent(path).absoluteString.removingPercentEncoding!
+        
+        var request = try URLRequest.init(url: url, method: method)
+        
+        return try URLEncoding.default.encode(request, with: parametters)
+    }
+}
